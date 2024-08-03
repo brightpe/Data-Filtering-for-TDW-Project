Fixed [2M+H]+ adduct calculation

CompTox_Score_MSDial_match.py
@@ -4,8 +4,8 @@
 ## For Tap Drinking Water NTA Project, Garcia-Jaramillo Lab at OSU 
 ################Input Start##################
 #### Notate your data file names####
-file = 'Human_Toxicity_example.csv'
-conc_file = 'MSDIAL_Conc_example.csv'
+file = 'Human_Toxicity - Copy.csv'
+conc_file = 'Updated_MSDIAL_Conc_profiles.csv'
 
 threshold = 100 ###This is the QC average intensity threshold (CPS) that features must be over in order to make the final output.
 ppm_threshold = 20 ###If comptox Monoisotopic MZ and measured MZ are available, this is the ppm threshold compounds must be under in order to make the final output.
@@ -80,28 +80,27 @@ total=len(list)
 hits = msdial_data[msdial_data['INCHIKEY'].isin(list)]
 first_slice = len(hits['INCHIKEY'])
 tox_duplicates = hits[hits['INCHIKEY'].duplicated()]
-print('\n{}'.format(first_slice),'of the total {} suspect compounds matched'.format(total),'including {} duplicates from the tox data\n'.format(len(tox_duplicates)))
+print('\n{}'.format(first_slice),'of the total {} suspect compounds matched'.format(total),'including {} duplicates from the tox data'.format(len(tox_duplicates)))
 
 
 
 
 #msdial_data=msdial_data.rename(columns={'CASRN':'CAS'})
 Tox_and_Conc_data = pd.merge(msdial_data, toxdata[['Total_CompTox_Score','CAS','Name','INCHIKEY']], on='INCHIKEY', how='left')
-print('\nResulting dataframe is {}'.format(len(Tox_and_Conc_data)),'rows after merging {} rows from MS-Dial output\n'.format(len(msdial_data)))
-print('\n--Matched {} Human Tox Scores to the Concentration Profile Data\n'.format(first_slice))
+print('\nResulting dataframe is {}'.format(len(Tox_and_Conc_data)),'rows after merging {} rows from MS-Dial output'.format(len(msdial_data)))
+print('\n--Matched {} Human Tox Scores to the Concentration Profile Data'.format(first_slice))
 
 
 
 lost_vals=toxdata[~toxdata['INCHIKEY'].isin(Tox_and_Conc_data['INCHIKEY'])]
 lost_vals[['INCHIKEY','PREFERRED_NAME','Total_CompTox_Score']]
 names = lost_vals['PREFERRED_NAME'].to_list()
-print('\nThese compounds were not matched to concentration profile data:\n{}\n\n'.format(names))
+print('\nThese compounds were not matched to concentration profile data:\n{}'.format(names))
 
 
 
 # Define function to map adduct masses for mass analysis. Values sourced from waters.com : https://support.waters.com/KB_Chem/Other/WKB67428_What_are_common_adducts_in_ESI_Mass_Spectrometry
-# Note: any [2M + x] adducts will not be calculated as the M mass will be needed.
-def adduct_mass(letter):
+def adduct_mass(letter, average_mass=None):
     if letter == '[M+H]+':
         return 1.0078
     elif letter == '[M+NH4]+':
@@ -124,35 +123,34 @@ def adduct_mass(letter):
         return 27.0027
     elif letter == '[M+Na-2H]-':
         return 20.9736
+    elif letter == '[2M+Na]+' and average_mass is not None:
+        return (average_mass / 2) + 22.9898
+    elif letter == '[2M+H]+' and average_mass is not None:
+        return (average_mass / 2) + 1.0078
     else:
         return 0  # Handle NaN values
-    
-Tox_and_Conc_data['Adduct Mass'] = Tox_and_Conc_data.apply(lambda row: sum(adduct_mass(letter) for letter in row), axis=1)
+#Tox_and_Conc_data['Adduct Mass'] = Tox_and_Conc_data.apply(lambda row: sum(adduct_mass(letter) for letter in row), axis=1)
+Tox_and_Conc_data['Adduct Mass'] = Tox_and_Conc_data.apply(lambda row: sum(adduct_mass(letter, row['Reference m/z']) for letter in row), axis=1)
+
 Tox_and_Conc_data.replace('', np.nan, inplace=True)
 Tox_and_Conc_data.replace(' ', np.nan, inplace=True)
 Tox_and_Conc_data['Absolute Mass Difference'] = abs((Tox_and_Conc_data['Reference m/z'].astype(float) - Tox_and_Conc_data['Adduct Mass'].astype(float))-Tox_and_Conc_data['MONOISOTOPIC_MASS'].astype(float))
 Tox_and_Conc_data['PPM Error From CompTox'] = abs((Tox_and_Conc_data['Absolute Mass Difference']/Tox_and_Conc_data['MONOISOTOPIC_MASS'].astype(float))*1e6)
-questionable = ['[2M+H]+', '[2M+Na]+']
-Tox_and_Conc_data.loc[Tox_and_Conc_data['Adduct type'].isin(questionable), ['Absolute Mass Difference','PPM Error From CompTox']] = 'No Calc'
-Tox_and_Conc_data.fillna('',inplace=True)
 
-print('\nUnfiltered Toxicity and Cocentration Profile dataset length: {}\n'.format(len(Tox_and_Conc_data)))
+print('\nUnfiltered Toxicity and Cocentration Profile dataset length: {}'.format(len(Tox_and_Conc_data)))
 Tox_and_Conc_data['QC average'] = Tox_and_Conc_data[['QC 1', 'QC 2', 'QC 3', 'QC 4']].mean(axis=1)
 
 Tox_and_Conc_data_prioritized = Tox_and_Conc_data[(Tox_and_Conc_data['QC average'] > threshold)]
-
-Tox_and_Conc_data_prioritized['PPM Error From CompTox'] = pd.to_numeric(Tox_and_Conc_data_prioritized['PPM Error From CompTox'], errors='coerce')
+Tox_and_Conc_data_prioritized.loc[:,'PPM Error From CompTox'] = pd.to_numeric(Tox_and_Conc_data_prioritized['PPM Error From CompTox'], errors='coerce')
 
 # Filter rows where 'PPM Error From CompTox' is less than the threshold or is NaN (i.e., blank in original data)
 Tox_and_Conc_data_prioritized = Tox_and_Conc_data_prioritized[(Tox_and_Conc_data_prioritized['PPM Error From CompTox'].isna()) | (Tox_and_Conc_data_prioritized['PPM Error From CompTox'] < ppm_threshold)]
 
 # Filter out rows containing 'Blank' in 'Spectrum reference file name'
-# Tox_and_Conc_data_prioritized = Tox_and_Conc_data_prioritized[(3*Tox_and_Conc_data_prioritized['average blank']) < (Tox_and_Conc_data_prioritized['average sample'])]
-# Tox_and_Conc_data_prioritized = Tox_and_Conc_data_prioritized[(3*Tox_and_Conc_data_prioritized['average ext. blank']) < (Tox_and_Conc_data_prioritized['average sample'])]
 Tox_and_Conc_data_prioritized = Tox_and_Conc_data_prioritized[~Tox_and_Conc_data_prioritized['Spectrum reference file name'].str.contains('Blank|QC|ISTD', case=False, na=False)]
 Tox_and_Conc_data_prioritized.sort_values(by=['Confidence level', 'QC average','average sample'], ascending=[True, True, False], inplace=True)
-print('\nFiltered Toxicity and Concentration profile dataset length: {} \n'.format(len(Tox_and_Conc_data_prioritized)))
+print('\nFiltered Toxicity and Concentration profile dataset length: {} '.format(len(Tox_and_Conc_data_prioritized)))
 
-Tox_and_Conc_data_prioritized.to_csv(r'Priority Compounds sorted from High Confidence List EXAMPLE.csv',index=False)
+Tox_and_Conc_data_prioritized.to_csv(r'Priority Compounds sorted from High Confidence List 08022024.csv',index=False)
 
 print('Successful\nSee ouput csv: Priority Compounds sorted from High Confidence List for results.\n=========End of Script=========')